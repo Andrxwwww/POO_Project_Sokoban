bda2fff5880e1e07adf7a52005f1d85bc31f66a3
Andrxwwww <117539407+Andrxwwww@users.noreply.github.com>
2023-11-26 14:17:40 +0000

    Delete Sokoban/src/pt/iscte/poo/sokobanstarter/NotMovableInterface.java

--- Sokoban/src/pt/iscte/poo/sokobanstarter/NotMovableInterface.java
+++
@@ -1,8 +0,0 @@
-package pt.iscte.poo.sokobanstarter;
-
-public interface NotMovableInterface {
-    
-    // para verificar se a empilhadora passa ou nao por um objeto NotMovable
-    public boolean doesElapse();
-
-}
